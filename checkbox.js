3a97f3b4110129e7971b4471c14dc9bbd488146d
Song <yousonghou@yahoo.com>
2018-10-20 19:43:44 +0000

    add checkbox logic that display value if checked

---
+++ checkbox.js
@@ -0,0 +1,19 @@
+function showWater1() {
+    document.getElementById('water1').value = 'water 1';
+}
+
+function showWater2() {
+    document.getElementById('water2').value = 'water 2';
+}
+
+function showWater3() {
+    document.getElementById('water3').value = 'water 3';
+}
+
+function showWater4() {
+    document.getElementById('water4').value = 'water 4';
+}
+
+function showWater5() {
+    document.getElementById('water5').value = 'water 5';
+}
